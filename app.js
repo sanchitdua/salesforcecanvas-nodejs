6a769a6466eb0cfb56f3823d2a858c6ea30a2c88
Bruno Fagundez <bfagundez@gmail.com>
2014-04-16 14:43:17 -0300

    fixed port

--- app.js
+++ app.js
@@ -88,6 +88,8 @@ app.post('/canvas', function(req, res){
   res.send('Hello World');
 });
 
-var server = app.listen(3000, function() {
+//app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
